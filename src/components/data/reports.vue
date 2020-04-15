c527919722a603dac1480ac3cee18efe36142805
xionghaiyan <1980333284@qq.com>
2020-04-15 10:59:04 +0800

    4.15

--- src/components/data/reports.vue
+++ src/components/data/reports.vue
@@ -6,16 +6,62 @@
       <el-breadcrumb-item>数据统计</el-breadcrumb-item>
       <el-breadcrumb-item>数据报表</el-breadcrumb-item>
     </el-breadcrumb>
+    <el-card>
+      <div id="main" style="width: 600px;height:400px;"></div>
+    </el-card>
   </div>
 </template>
 
 <script>
+import echarts from 'echarts'
 export default {
   name: 'statistics',
-  data () {
-    return {}
+  mounted () {
+    var myChart = echarts.init(document.getElementById('main'))
+    this.$http.get('reports/type/1').then(response => {
+      var {data} = response
+      if (data.meta.status !== 200) {
+        return this.$message.error('获取折线图数据失败')
+      }
+      myChart.setOption(data.data)
+    })
   },
-  methods: {},
+  data () {
+    return {
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  methods: {
+  },
   computed: {}
 }
 </script>
